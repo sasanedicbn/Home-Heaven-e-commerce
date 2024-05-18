bb7148fa8c35c69468324a0fa2f89c7822fbfdfe
Sasa Nedic <svetasmak0@gmail.com>
2024-05-19 00:03:49 +0200

    Add logos

--- src/pages/Featured.tsx
+++ src/pages/Featured.tsx
@@ -4,7 +4,11 @@ const Featured = () => {
             <div className="container-featured-products">
              <h2 className="heading-featured">As featured in</h2>
              <div className="logos">
-
+               <img src="public/Logos/usaToday.56382ee08e942ad7c93c.png" alt="logo"/>
+               <img src="public/Logos/techCrunch.8e59ce33cdb22ba0b8dd.png" alt="logo"/>
+               <img src="public/Logos/newYorkTimes.1279b92266cf081e20a6.png" alt="logo"/>
+               <img src="public/Logos/forbes.c9d98237f5d5f000eaad.png" alt="logo"/>
+               <img src="public/Logos/businessInsider.a50839ce9f675fc574e6.png" alt="logo"/>
              </div>
             </div>
         </section>
