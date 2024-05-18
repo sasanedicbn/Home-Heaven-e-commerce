f2c03425cc4f16f48f5106e7939cbac82e1abf76
Sasa Nedic <svetasmak0@gmail.com>
2024-05-18 14:02:46 +0200

    Create NavBar component

---
+++ src/pages/NavBar.tsx
@@ -0,0 +1,35 @@
+import { BsBag } from 'react-icons/bs';
+const NavBar = () => {
+    return(
+    <section>
+        <nav>
+            {/* ZAMJENITI OVO A POSLIJE SA <LINK TO="/"/> */}
+            <a className="nav-list-item-link" href="/">
+                <div className="logo-div">
+                  <BsBag/>
+                  <span className='logo-text'>Home Heaven</span>
+                </div>
+            </a>
+            <ul className='nav-list'>
+                <a className='nav-list-item-link active-link'href='/'>
+                   <li className="nav-list-item">Home</li>
+                </a>
+                <a className='nav-list-item-link' href='/products'>
+                   <li className="nav-list-item">Products</li>
+                </a>
+            </ul>
+            <div className='shopping-card'>
+              <div className='icon-text'>
+                <span>Cart</span>
+                  <a href='/' className='cart-icon'>
+                  <BsBag/>
+                  </a>
+              </div>
+               <span className="badge">0</span>
+            </div>
+        </nav>
+    </section>
+
+    )
+} 
+export default NavBar;
