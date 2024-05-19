27a7f2101d3d7ab68e9e3f2edb449d13d9ee5c33
Sasa Nedic <svetasmak0@gmail.com>
2024-05-20 00:14:24 +0200

    Change code in Creeds

--- src/pages/Creeds.tsx
+++ src/pages/Creeds.tsx
@@ -1,13 +1,35 @@
+
+import { FaStar, FaEye, FaBook } from 'react-icons/fa';
+import CreedsItem from '../components/CreedsItem';
+
 const Creeds = () => {
     return(
         <section className="section-creeds">
             <div className="container-creeds">
-             <div className="creeds-header">
-              <h3 className="header-title">Creeds we live by</h3>
-              <h4 className="header-description-creeds">Elevate your home with our attitude of excellence and timeless style.</h4>
-             </div>
+                <div className="creeds-header">
+                    <h3 className="header-title">Creeds we live by</h3>
+                    <h4 className="header-description-creeds">Elevate your home with our attitude of excellence and timeless style.</h4>
+                </div>
+                <div className="creeds-items">
+                    <CreedsItem 
+                        icon={FaStar}
+                        title="Radiance"
+                        description="Our commitment to providing sparkling solutions ensures every customer enjoys a shining, glowing experience with unmatched quality."
+                    />
+                    <CreedsItem 
+                        icon={FaEye}
+                        title="Clarity"
+                        description="With clear vision and keen insight, we guide customers down the path to lasting success with exceptional service and anticipation."
+                    />
+                    <CreedsItem 
+                        icon={FaBook}
+                        title="Heritage"
+                        description="As a trusted partner, we offer expert guidance, drawing on our deep wisdom and wealth of experience to ensure satisfaction and success."
+                    />
+                </div>
             </div>
         </section>
     )
 }
+
 export default Creeds;
