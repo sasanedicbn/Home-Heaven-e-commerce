3c6ffd7f89fb47503dc85e6356cf19bb8219470e
Sasa Nedic <svetasmak0@gmail.com>
2024-05-19 23:20:25 +0200

    Finish style for feeturedProducts

--- src/pages/FeaturedProducts.tsx
+++ src/pages/FeaturedProducts.tsx
@@ -8,9 +8,16 @@ const FeaturedProducts = () => {
            <h3 className="featured-header-title">FEATURED PRODUCTS</h3>
            <h4 className="featured-header-description">The art of modern living unlocked.</h4>
           </div>
+          <div className="feturedItem">
            <FeaturedItem  name="Sofa Set" price="$1,299.99" img="public/FeturedProducts/product-1.jpeg"/>
            <FeaturedItem  name="Utopia Sofa" price="$799.99" img="public/FeturedProducts/product-16.jpeg"/>
            <FeaturedItem  name="Suede Armchair" price="$159.99" img="public/FeturedProducts/product-17.jpeg"/>
+          </div>
+          <div className="featured-btn-container">
+            <a href="/">
+                <button className="btn btn-featured">All Products</button>
+            </a>
+          </div>
          </div>
         </section>
     )
