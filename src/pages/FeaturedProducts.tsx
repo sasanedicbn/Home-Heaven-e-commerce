464537c5faf64b19fe83418ba8577baca836b3b5
Sasa Nedic <svetasmak0@gmail.com>
2024-05-21 22:53:58 +0200

    Clean code

--- src/pages/FeaturedProducts.tsx
+++ src/pages/FeaturedProducts.tsx
@@ -1,25 +1,27 @@
+import React from 'react';
 import FeaturedItem from "../components/FeaturedItem";
 
-const FeaturedProducts = ({targetRef}) => {
-    return(
-        <section className="section-featured-products" ref={targetRef}>
-         <div className="container-featured-products">
-          <div className="featured-header">
-           <h3 className="featured-header-title">FEATURED PRODUCTS</h3>
-           <h4 className="featured-header-description">The art of modern living unlocked.</h4>
-          </div>
-          <div className="feturedItem">
-           <FeaturedItem  name="Sofa Set" price="$1,299.99" img="public/FeturedProducts/product-1.jpeg"/>
-           <FeaturedItem  name="Utopia Sofa" price="$799.99" img="public/FeturedProducts/product-16.jpeg"/>
-           <FeaturedItem  name="Suede Armchair" price="$159.99" img="public/FeturedProducts/product-17.jpeg"/>
-          </div>
-          <div className="featured-btn-container">
-            <a href="/">
-                <button className="btn btn-featured">All Products</button>
-            </a>
-          </div>
-         </div>
+const FeaturedProducts = React.forwardRef((props, ref) => {
+    return (
+        <section className="section-featured-products" ref={ref}>
+            <div className="container-featured-products">
+                <div className="featured-header">
+                    <h3 className="featured-header-title">FEATURED PRODUCTS</h3>
+                    <h4 className="featured-header-description">The art of modern living unlocked.</h4>
+                </div>
+                <div className="feturedItem">
+                    <FeaturedItem name="Sofa Set" price="$1,299.99" img="public/FeturedProducts/product-1.jpeg" />
+                    <FeaturedItem name="Utopia Sofa" price="$799.99" img="public/FeturedProducts/product-16.jpeg" />
+                    <FeaturedItem name="Suede Armchair" price="$159.99" img="public/FeturedProducts/product-17.jpeg" />
+                </div>
+                <div className="featured-btn-container">
+                    <a href="/">
+                        <button className="btn btn-featured">All Products</button>
+                    </a>
+                </div>
+            </div>
         </section>
-    )
-}
+    );
+});
+
 export default FeaturedProducts;
