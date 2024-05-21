6ab5ecacd351344b40e1ac1e978b39efc89608a2
Sasa Nedic <svetasmak0@gmail.com>
2024-05-22 00:40:00 +0200

    Create react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 
 
 
-function App() {
 
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    }
+  ])
   return (
-    <>
-    <Home/>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
