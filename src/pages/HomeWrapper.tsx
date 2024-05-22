d14bf717c59ab17585a7cfeedaa18aa0c322e7ab
Sasa Nedic <svetasmak0@gmail.com>
2024-05-22 23:50:49 +0200

    Create HomeWrapper component

---
+++ src/pages/HomeWrapper.tsx
@@ -0,0 +1,12 @@
+import '../index.css'
+const HomeWrapper = ({children, paddingTop = "16px"}) => {
+    const style = {
+        paddingTop: paddingTop
+    }
+    return(
+     <div className='wrapper' style={style}>
+
+     </div>
+    )
+}
+export default HomeWrapper;
