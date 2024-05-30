7086af555bc179f11b0958d22782006b45191d14
Sasa Nedic <svetasmak0@gmail.com>
2024-05-30 09:02:33 +0200

    Create chagnes in Testimonila components

--- src/pages/Testimonials.tsx
+++ src/pages/Testimonials.tsx
@@ -1,19 +1,33 @@
+import { useState } from "react";
 import HomeSectionWrapper from "./HomeSectionWrapper";
 import { testimonialData } from "./constants/constants";
 
 const Testimonials = () => {
+    const [activeSlider, setActiveSlider] = useState(1);
+
     return (
-        <HomeSectionWrapper title="TESTIMONIALS" subTitle="Discover the stories of our delighted customers - thousands and counting!">
-            <section className="section-testimonials">
-                {testimonialData.map(testimonial => (
-                    <div key={testimonial.id} className="testimonial-card">
-                        <img src={testimonial.img} alt={`Testimonial from ${testimonial.name}`} className="testimonial-img" />
-                        <h3 className="testimonial-title">{testimonial.title}</h3>
-                        <p className="testimonial-description">{testimonial.description}</p>
-                        <p className="testimonial-name">{testimonial.name}</p>
-                        <p className="testimonial-location">{testimonial.location}</p>
-                    </div>
-                ))}
+        <HomeSectionWrapper 
+            title="TESTIMONIALS" 
+            subTitle="Discover the stories of our delighted customers - thousands and counting!"
+        >
+            <section className="">
+                <div className="slider">
+                    {testimonialData.map(testimonial => (
+                        activeSlider === testimonial.id && (
+                            <div key={testimonial.id} className="testimonial-card">
+                                <h3 className="testimonial-title">{testimonial.title}</h3>
+                                <p className="testimonial-description">{testimonial.description}</p>
+                                <img 
+                                    src={testimonial.img} 
+                                    alt={`Testimonial from ${testimonial.name}`} 
+                                    className="testimonial-img" 
+                                />
+                                <p className="testimonial-name">{testimonial.name}</p>
+                                <p className="testimonial-location">{testimonial.location}</p>
+                            </div>
+                        )
+                    ))}
+                </div>
             </section>
         </HomeSectionWrapper>
     );
