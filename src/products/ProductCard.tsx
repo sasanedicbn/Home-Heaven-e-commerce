548413877eb38c98288a867abd33d4d344c27bce
Sasa Nedic <svetasmak0@gmail.com>
2024-06-02 21:51:58 +0200

    Finish style for ProductsContainer

--- src/products/ProductCard.tsx
+++ src/products/ProductCard.tsx
@@ -5,7 +5,7 @@ const ProductCard = ({ product }) => {
         <article className="product-card">
             <img src={image} alt={name} />
             <div className="product-info">
-                <h2>{name}</h2>
+                <h5>{name}</h5>
                 <span className="price">${(price / 100).toFixed(2)}</span>
             </div>
             <p className="description">{description}</p>
