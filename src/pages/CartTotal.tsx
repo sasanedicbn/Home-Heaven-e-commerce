df841401356979e623f530b8dde88afc4f7a12db
Sasa Nedic <svetasmak0@gmail.com>
2024-06-09 01:00:57 +0200

    Create shippingFee

--- src/pages/CartTotal.tsx
+++ src/pages/CartTotal.tsx
@@ -1,31 +1,35 @@
 import { useSelector } from "react-redux";
 
 const CartTotal = () => {
-    const products = useSelector(state => state.cart.cart)
+    const products = useSelector(state => state.cart.cart);
 
     const subTotal = products.reduce((total, product) => {
-        return total + (product.price * product.quantity)
-    }, 0)
-    
-    const shippingFee = 800; 
-    const orderTotal = subTotal + shippingFee
+        return total + (product.price * product.quantity);
+    }, 0);
 
-    console.log('carttotalKOMPONENT',subTotal)
-    return(
+    const shippingFee = products.reduce((total, product) => {
+        return total + (product.shipping ? 0 : 800); 
+    }, 0);
+
+    const orderTotal = subTotal + shippingFee;
+
+    console.log('carttotalKOMPONENT', subTotal);
+    return (
         <div className="cart-total">
             <div>
-              <p className="cart-total-subtotal">SubTotal: </p>
-              <span>${(subTotal/100).toFixed(2)}</span>
+                <p className="cart-total-subtotal">SubTotal: </p>
+                <span>${(subTotal / 100).toFixed(2)}</span>
             </div>
             <div>
-              <p className="cart-total-fee">Shipping fee: </p>
-              <span>${(shippingFee /100).toFixed(2)}</span>
+                <p className="cart-total-fee">Shipping fee: </p>
+                <span>${(shippingFee / 100).toFixed(2)}</span>
             </div>
             <div>
-              <p className="orderTotal">OrderTotal: </p>
-              <span>${(orderTotal / 100).toFixed(2)}</span>
+                <p className="orderTotal">OrderTotal: </p>
+                <span>${(orderTotal / 100).toFixed(2)}</span>
             </div>  
         </div>
-    )
-}
+    );
+};
+
 export default CartTotal;
