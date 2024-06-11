b4e619ab591d32728c744aa1e443e9133289d794
Sasa Nedic <svetasmak0@gmail.com>
2024-06-11 17:10:06 +0200

    Create changes in productsFilters

--- src/store/productsFilters.ts
+++ src/store/productsFilters.ts
@@ -1,6 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Product } from "../types/types";
 
-const initialFilters = {
+
+
+type  Filters = {
+    category: string;
+    company: string;
+    color: string;
+    search: string;
+    price: number;
+    shipping: boolean;
+    sort: string;
+    [key: string]: string | number | boolean;
+
+}
+
+
+interface FiltersState {
+    products: Product[];
+    filters: Filters;
+    filteredProducts: Product[];
+}
+
+const initialFilters: Filters = {
     category: "All",
     company: "All",
     color: "All",
@@ -10,53 +32,51 @@ const initialFilters = {
     sort: "lowest",
 };
 
-
-
+const initialState: FiltersState = {
+    products: [],
+    filters: initialFilters,
+    filteredProducts: [],
+};
 
 const productsFiltersSlice = createSlice({
     name: 'filters',
-    initialState: {
-        products: [],
-        filters: initialFilters,
-        filteredProducts: [],
-    },
+    initialState,
     reducers: {
-        setProducts: (state, action) => {
+        setProducts: (state, action: PayloadAction<Product[]>) => {
             state.products = action.payload;
             state.filteredProducts = action.payload;
         },
-        setFilter: (state, action) => {
-            console.log('action',action)
+        setFilter: (state, action: PayloadAction<{ key: string; value: any }>) => {
             state.filters[action.payload.key] = action.payload.value;
-            console.log(action)
             state.filteredProducts = filterProducts(state.products, state.filters);
-        
         },
-        resetFilters:(state) => {
-            state.filters = initialFilters
-            state.filteredProducts = state.products
+        resetFilters: (state) => {
+            state.filters = initialFilters;
+            state.filteredProducts = state.products;
         },
-        sortProduct:(state,action) => {
-            state.filters.sort = action.payload
-            state.filteredProducts = sortProducts(state.filteredProducts, state.filters.sort)
+        sortProduct: (state, action: PayloadAction<string>) => {
+            state.filters.sort = action.payload;
+            state.filteredProducts = sortProducts(state.filteredProducts, state.filters.sort);
         }
     },
 });
 
-const sortProducts = (filteredProducts, sort) =>{
-    switch(sort){
+const sortProducts = (filteredProducts: Product[], sort: string) => {
+    switch (sort) {
         case 'lowest':
-            return [...filteredProducts].sort((a,b) => a.price - b.price);
+            return [...filteredProducts].sort((a, b) => a.price - b.price);
         case 'highest':
-            return [...filteredProducts].sort((a,b) => b.price - a.price);
+            return [...filteredProducts].sort((a, b) => b.price - a.price);
         case 'a-z':
-            return [...filteredProducts].sort((a,b) => a.name.localeCompare(b.name));
+            return [...filteredProducts].sort((a, b) => a.name.localeCompare(b.name));
         case 'z-a':
-            return [...filteredProducts].sort((a,b) => b.name.localeCompare(a.name))
+            return [...filteredProducts].sort((a, b) => b.name.localeCompare(a.name));
+        default:
+            return filteredProducts;
     }
 }
 
-const filterProducts = (products, filters) => {
+const filterProducts = (products: Product[], filters: Filters) => {
     return products.filter(product => {
         return (
             (filters.category === 'All' || product.category === filters.category) &&
@@ -69,5 +89,5 @@ const filterProducts = (products, filters) => {
     });
 };
 
-export const { setFilter, setProducts, resetFilters, sortProduct} = productsFiltersSlice.actions;
+export const { setFilter, setProducts, resetFilters, sortProduct } = productsFiltersSlice.actions;
 export default productsFiltersSlice.reducer;
